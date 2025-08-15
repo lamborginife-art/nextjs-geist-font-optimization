fb85355b431827114e40c4b675abc52ad73068f6
lamborginife-art <lamborgini.fe@gmail.com>
2025-08-15 12:38:01 -0400

    Add .next/static/chunks/src_app_signup_page_tsx_a1f9e366._.js 1755275879930

---
+++ .next/static/chunks/src_app_signup_page_tsx_a1f9e366._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_c9c3192c._.js",
+  "static/chunks/src_68fe7f8e._.js"
+],
+    source: "dynamic"
+});
